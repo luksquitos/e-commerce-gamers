25e71a6576885515caf8a5c9a10094df1a34e807
Lucas <lucasmartinscaet@hotmail.com>
2023-02-10 16:48:57 -0300

    Arquivos estáticos usados na URL

--- app/core/urls.py
+++ app/core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework.routers import DefaultRouter
 
 
@@ -32,4 +34,4 @@ urlpatterns = [
     path(
         'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
     )
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
