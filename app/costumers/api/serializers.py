052083680bfc2864b0aa4be0382f9ea6fee405a8
Lucas <lucasmartinscaet@hotmail.com>
2023-02-11 19:20:45 -0300

    feat: Criando os serializers para Costumer, Purchase

--- app/costumers/api/serializers.py
+++ app/costumers/api/serializers.py
@@ -1,1 +1,38 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
+from costumers.models import Costumer, Purchase, PurchaseProduct
+
+
+class CostumerSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    def create(self, **validated_data):
+        password = validated_data.pop('password')
+        email = validated_data.get('email')
+        
+        user = User.objects.create(username=email)
+        user.set_password(password)
+        
+        costumer = Costumer.objects.create(
+            user=user, **validated_data
+        )
+        return costumer
+
+    class Meta:
+        model = Costumer
+        fields = "__all__"
+        
+
+
+class PurchaseProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PurchaseProduct
+        fields = "__all__"
+
+
+class PurchaseSerializer(serializers.ModelSerializer):
+    products = PurchaseProductSerializer(read_only=True)
+
+    class Meta:
+        model = Purchase
+        fields = "__all__"
--- app/costumers/models.py
+++ app/costumers/models.py
@@ -6,12 +6,21 @@ from products.models import Product
 class Costumer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     name = models.CharField("Nome", max_length=100)
+    email = models.EmailField("Email")
+    
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = "Cliente"
+        verbose_name_plural = "Clientes"
 
 
 class Purchase(models.Model):
     received = models.BooleanField("Compra entregue?")
     costumer = models.ForeignKey(Costumer, on_delete=models.DO_NOTHING)
     date_time_purchase = models.DateTimeField("Data e hora de compra", auto_now_add=True)
+    date_time_received = models.DateTimeField("Data e hora de recebimento", null=True)
     
 
 class PurchaseProduct(models.Model):
