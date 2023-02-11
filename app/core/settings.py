b53bf1cacf0c99766fe83b37e12cc6b50981a858
Lucas <lucasmartinscaet@hotmail.com>
2023-02-11 13:47:55 -0300

    Deixando o tema branco apenas para processo de desenvolvimento

--- app/core/settings.py
+++ app/core/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'products',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,7 +42,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
-    'products',
 ]
 
 MIDDLEWARE = [
