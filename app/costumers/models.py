272770b4dd098af5a336082500844c5642483b91
Lucas <lucasmartinscaet@hotmail.com>
2023-02-11 18:32:09 -0300

    feat: Criando a base de dados para o costumer e compras

--- app/costumers/models.py
+++ app/costumers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import Product
 
-# Create your models here.
+
+class Costumer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField("Nome", max_length=100)
+
+
+class Purchase(models.Model):
+    received = models.BooleanField("Compra entregue?")
+    costumer = models.ForeignKey(Costumer, on_delete=models.DO_NOTHING)
+    date_time_purchase = models.DateTimeField("Data e hora de compra", auto_now_add=True)
+    
+
+class PurchaseProduct(models.Model):
+    purchase = models.ForeignKey(Purchase, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    
+    #TODO
+    # O método save será sobrescrito para 
+    # tratar a quantidade presente?
+    # melhor ser pelo viewset
+    
+
